4082ac3be26009fa868c6b337637954078bfdadf
urielcuellararenas <uriel-rl@hotmail.com>
2019-12-02 13:04:44 -0500

    php

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Las 3 metaetiquetas anteriores * deben * ser lo primero en la cabeza; cualquier otro contenido principal debe venir * después * de estas etiquetas -->
+  <title>PLANTILLA | WEB 4</title>
+
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link href="css/animate.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+
+  <div class="gallery">
+    <div class="text-center">
+      <h2>Gallery</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quod voluptate consequuntur ad quasi, dolores obcaecati ex aliquid, dolor provident </p>
+    </div>
+    <div class="container">
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/8.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>sweet marley</h4>
+            <p>Marley tried to convince her but she was not interested.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/9.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>sweet marley</h4>
+            <p>Marley tried to convince her but she was not interested.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/10.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>sweet marley</h4>
+            <p>Marley tried to convince her but she was not interested.</p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/11.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>sweet marley</h4>
+            <p>Marley tried to convince her but she was not interested.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/12.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>sweet marley</h4>
+            <p>Marley tried to convince her but she was not interested.</p>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="col-md-4">
+        <figure class="effect-marley">
+          <img src="img/13.jpg" alt="" class="img-responsive" />
+          <figcaption>
+            <h4>sweet marley</h4>
+            <p>Marley tried to convince her but she was not interested.</p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="js/jquery.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script>
+    wow = new WOW({}).init();
+  </script>
+</body>
+
+</html>
